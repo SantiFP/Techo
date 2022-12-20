05d0d794698d38ca962ff2705e517087d749b3aa
unknown <santifp1997@gmail.com>
2022-12-19 23:26:09 -0300

    update

--- src/pages/Budget/Modal/Modal.js
+++ src/pages/Budget/Modal/Modal.js
@@ -3,27 +3,26 @@ import ReactDOM from "react-dom";
 import ModalVector from "../Assets/Vector-modal.png";
 import Separation from "../Assets/separador.png";
 import Cross from "../Assets/cross.png";
-import {useState} from 'react';
+import { useState } from "react";
 
 const Backdrop = (props) => {
   return <div onClick={props.onClick} className={Styles.backdrop} />;
 };
 
 const ModalOverlay = (props) => {
-
-  const [files,setFile] = useState();
+  const [files, setFile] = useState();
 
   const onLoad = (e) => {
     const files = e.target.files;
-    let text = '';
+    let text = "";
     if (files.length !== 0) {
       for (let i = 0; i < files.length; i++) {
         text += `${files[i].name}
       `;
       }
-      setFile(text); 
-    } 
-  }
+      setFile(text);
+    }
+  };
   return (
     <div className={Styles.modal}>
       <div onClick={props.onClick} className={Styles.cross}>
@@ -31,18 +30,28 @@ const ModalOverlay = (props) => {
       </div>
       <div className={Styles.flexModal}>
         <p className={Styles.dragText}>ARRASTRAR Y SOLTAR ARCHIVO DE CARGA</p>
-        <label className={Styles.drag}> 
-          <input onChange={onLoad} className={Styles.fileInput} type="file" multiple />
+        <label className={Styles.drag}>
+          <input
+            onChange={onLoad}
+            className={Styles.fileInput}
+            type="file"
+            multiple
+          />
           <div className={Styles.fileImg}>
             <img alt="modal" src={ModalVector} />
-          </div> 
+          </div>
         </label>
         <label className={Styles.modalButton}>
-          <input onChange={onLoad} className={Styles.none} type="file" multiple />
+          <input
+            onChange={onLoad}
+            className={Styles.none}
+            type="file"
+            multiple
+          />
           SELECCIONAR ARCHIVO DESDE LA PC
         </label>
 
-        <span style={ { fontWeight: '500' } }>{files}</span>
+        <span style={{ fontWeight: "500" }}>{files}</span>
 
         <img alt="sep" src={Separation} />
         <p className={Styles.download}>
